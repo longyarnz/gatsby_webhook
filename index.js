b18b80f90c8f50f6dc8f51e860fc46d1dcfb0bfb
Ayodele Olalekan <longyarnz@gmail.com>
2020-01-15 10:18:28 +0100

    Add health check metrics to server

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const statusMonitor = require('express-status-monitor')();
 
 // Initialize express server
 const app = express();
+
+// Add health check metrics to server
+app.use(statusMonitor);
+
 app.use(express.json());
 const PORT = process.env.PORT || 4000;
+
+// Expose a default get route tp check current server stats
+app.get('/', statusMonitor.pageRoute);
 
 // Run server.
 app.listen(PORT, () => {
