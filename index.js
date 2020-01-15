a8ea87ad3c861befc1aeeeee949e3fb9cabfa03a
Ayodele Olalekan <longyarnz@gmail.com>
2020-01-15 10:38:22 +0100

    Add a users route to fetch data from github

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const statusMonitor = require('express-status-monitor')();
 
 // Initialize express server
@@ -12,6 +13,27 @@ const PORT = process.env.PORT || 4000;
 
 // Expose a default get route tp check current server stats
 app.get('/', statusMonitor.pageRoute);
+
+async function pullUsers() {
+    const names = ['longyarnz', 'gearon'];
+    try {
+        console.log('Pulling users from GitHub');
+        const requests = names.map(async name => {
+            const fetchUsers = await fetch(`https://api.github.com/users/${name}`);
+            return await fetchUsers.json();
+        });
+        return await Promise.all(requests);
+    } catch (error) {
+        console.log(error);
+        return 'Unable to fetch users';
+    }
+}
+
+// Get users from GitHub
+app.get('/users', async (req, res) => {
+    const users = await pullUsers();
+    res.json(users);
+})
 
 // Run server.
 app.listen(PORT, () => {
